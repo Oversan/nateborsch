593c863995f94ac5bcd7dcad2ecbe26b4a325f93
Anton Borsch <anton.borsch@gmail.com>
2016-06-18 15:43:25 +0300

    Generated static files

---
+++ static/js/bundle.js
@@ -0,0 +1,37 @@
+$(function () {
+  $('.magnetic-box').magnificPopup({
+    type: 'image',
+    removalDelay: 500,
+    closeOnContentClick: true,
+    showCloseBtn: false,
+    preloader: true,
+    autoFocusLast: false,
+    mainClass: 'mfp-fade'
+  })
+
+  $('.magnetic-gallery').each(function() {
+    $(this).magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      removalDelay: 500,
+      closeOnContentClick: true,
+      showCloseBtn: false,
+      preloader: true,
+      autoFocusLast: false,
+      mainClass: 'mfp-fade',
+      gallery: {
+        enabled: true,
+        arrowMarkup: ''
+      }
+    })
+  })
+
+  $('.open-popup-link').magnificPopup({
+    type: 'inline',
+    removalDelay: 500,
+    showCloseBtn: false,
+    midClick: true,
+    autoFocusLast: false,
+    mainClass: 'mfp-fade'
+  })
+})
