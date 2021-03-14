f5839eda30882f3b83c95a2bdd6db8bbf1e6f377
Anton Borsch <anton.borsch@gmail.com>
2021-03-14 18:50:46 +0100

    Added panorama for Koi koi project.

--- static/js/bundle.js
+++ static/js/bundle.js
@@ -1,3 +1,23 @@
+var koiKoiImages = [
+  'static/img/static/vis/koi_koi/koi_koi_1_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_2_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_3_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_4_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_5_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_6_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_7_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_8_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_9_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_10_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_11_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_12_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_13_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_14_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_15_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_16_big.jpg',
+  'static/img/static/vis/koi_koi/koi_koi_17_big.jpg',
+];
+
 $(function () {
   $('.magnetic-box').magnificPopup({
     type: 'image',
@@ -20,11 +40,29 @@ $(function () {
       autoFocusLast: false,
       mainClass: 'mfp-fade',
       gallery: {
-        enabled: true,
-        arrowMarkup: ''
+        enabled: true
       }
     })
   })
+
+  $('#koi-koi-gallery').magnificPopup({
+    items: [
+      {
+        src: '/panorama-livingroom.html',
+        type: 'iframe'
+      },
+      ...koiKoiImages.map(src => ({
+        src,
+        type: 'image'
+      }))
+    ],
+    gallery: {
+      enabled: true,
+    },
+    type: 'image',
+    showCloseBtn: false,
+    preloader: true,
+  });
 
   $('.open-popup-link').magnificPopup({
     type: 'inline',
